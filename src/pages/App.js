25ac9719bd8f1b6233bd39059ff873349d4e96c0
abhikumar22 <abhi.ckp1002@gmail.com>
2020-05-09 17:58:59 +0530

    upload form

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,30 +1,105 @@
 import React from "react";
 import "../assets/css/style.css";
-import { STRINGS } from "../utils/constants";
+import {
+  STRINGS,
+  MONTHLY_RENTAL_AMOUNT_OPTIONS,
+  ZIPCODE_OPTIONS,
+  CUSTOMER_AGE_OPTIONS,
+  PRODUCT_NAME_OPTIONS,
+  RENTAL_TENURE_OPTIONS
+} from "../utils/constants";
 import Select from 'react-select';
+import { Slider } from '@material-ui/core';
 
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' },
-];
+
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       backgroundcolor: "transparent",
-      selectedOption: null,
+      selectedMonthlyRentalAmountOption: null,
+      selectedZipcodeOption: null,
+      selectedAgeOption: null,
+      selectedTenureOption: null,
+      selectedProductOption: null,
+      newValue: []
     };
     this.landingDiv = React.createRef();
     this.homeDiv = React.createRef();
     this.profileDiv = React.createRef();
+    this.monthlyRentalAmountOptions = [
+      { value: 1, label: '0 - $1000' },
+      { value: 2, label: '$1001 - $2000' },
+      { value: 3, label: '$2001 - $3000' },
+      { value: 4, label: '$3001 - $4000' },
+      { value: 5, label: '$4001 - $5000' },
+      { value: 6, label: 'more than 5001$' },
+    ];
+    this.zipcodeOptions = [
+      { value: '35801', label: 'Alabama 35801' },
+      { value: '99501', label: 'Alaska 99501' },
+      { value: '85001', label: 'Arizona 85001' },
+      { value: '94203', label: 'Arkansas 94203' },
+      { value: '90001', label: 'California 90001' },
+
+    ];
+    this.customerAgeOptions = [
+      { value: 1, label: 'less than 21' },
+      { value: 2, label: 'greater than 21' },
+    ];
+    this.productsOptions = [
+      { value: 1, label: 'TV' },
+      { value: 2, label: 'Washing Machine' },
+      { value: 3, label: 'Air Conditioner' },
+    ];
+    this.rentalTenureOptions = [
+      { value: [0, 3], label: '0 - 3' },
+      { value: [3, 6], label: '3 - 6' },
+      { value: [6, -1], label: 'more than 6 months' },
+
+    ];
   }
 
-  handleChange = selectedOption => {
+
+  handleChange = (event, newValue) => {
+    this.setState({ newValue }, () => {
+      // console.log("vall",newValue)
+    });
+  };
+
+
+  handleChangeRental = (selectedMonthlyRentalAmountOption) => {
     this.setState(
-      { selectedOption },
-      () => console.log(`Option selected:`, this.state.selectedOption)
+      { selectedMonthlyRentalAmountOption },
+      () => console.log(`Option selected:`, this.state.selectedMonthlyRentalAmountOption)
+    );
+  };
+  handleChangeZipcode = (selectedZipcodeOption) => {
+    this.setState(
+      { selectedZipcodeOption },
+      () => console.log(`Option selected:`, this.state.selectedZipcodeOption)
+    );
+  };
+
+  handleChangeAge = (selectedAgeOption) => {
+    this.setState(
+      { selectedAgeOption },
+      () => console.log(`Option selected:`, this.state.selectedAgeOption)
+    );
+  };
+
+  handleChangeProduct = (selectedProductOption) => {
+    this.setState(
+      { selectedProductOption },
+      () => console.log(`Option selected:`, this.state.selectedProductOption)
+    );
+  };
+
+  handleChangeTenure = (selectedTenureOption) => {
+    this.setState(
+      { selectedTenureOption },
+      () => console.log(`Option selected:`, this.state.selectedTenureOption)
     );
   };
 
@@ -126,16 +201,53 @@ export default class App extends React.Component {
                   <div className="formBorder py-5 px-5">
                     <form className="w-100 text-center">
                       <div className="form-group">
-                        <p>Monthly Rental Amount( $ ) :</p>
+
+                        <div className="py-1 row">
+                          <div className="col-6">
+                            <p>{STRINGS.MONTHLY_RENTAL_AMOUNT}</p>
+                          </div>
+                          <div className="col-6">
+                            <Select
+                              value={this.state.selectedMonthlyRentalAmountOption}
+                              onChange={this.handleChangeRental}
+                              options={MONTHLY_RENTAL_AMOUNT_OPTIONS}
+                              isClearable={true}
+                              isSearchable={false}
+                            />
+                          </div>
+                        </div>
+                        <p>{STRINGS.CUSTOMER_AGE}</p>
                         <Select
-                          value={this.state.selectedOption}
-                          onChange={this.handleChange}
-                          options={options}
+                          value={this.state.selectedAgeOption}
+                          onChange={this.handleChangeAge}
+                          options={this.customerAgeOptions}
+                          isClearable={true}
+                          isSearchable={false}
                         />
-                        <p>Customer Age ( Years ) :</p>
-                        <p>Zipcode :</p>
-                        <p>Product Name :</p>
-                        <p>Rental Tenure :</p>
+                        <p>{STRINGS.ZIPCODE}</p>
+                        <Select
+                          value={this.state.selectedZipcodeOption}
+                          onChange={this.handleChangeZipcode}
+                          options={this.zipcodeOptions}
+                          isClearable={true}
+                          isSearchable={true}
+                        />
+                        <p>{STRINGS.PRODUCT_NAME}</p>
+                        <Select
+                          value={this.state.selectedProductOption}
+                          onChange={this.handleChangeProduct}
+                          options={this.productsOptions}
+                          isClearable={true}
+                          isSearchable={true}
+                        />
+                        <p>{STRINGS.RENTAL_TENURE}</p>
+                        <Select
+                          value={this.state.selectedTenureOption}
+                          onChange={this.handleChangeTenure}
+                          options={this.rentalTenureOptions}
+                          isClearable={true}
+                          isSearchable={false}
+                        />
                       </div>
                       <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,9 +3,52 @@ const STRINGS = {
   NAV_TITLE:"CasaOne",
   NAV_HOME:"Add Rules",
   NAV_PROFILE:"View Rules",
+  RENTAL_TENURE:"Rental Tenure(in Months.)",
+  PRODUCT_NAME:"Product Name",
+  ZIPCODE:"Zipcode",
+  CUSTOMER_AGE:"Customer Age(in Years.)",
+  MONTHLY_RENTAL_AMOUNT:"Monthly Rental Amount (in Dollars $)",
+
+
+
+
 };
 
+const MONTHLY_RENTAL_AMOUNT_OPTIONS = [
+  { value: 1, label: '0 - $1000' },
+  { value: 2, label: '$1001 - $2000' },
+  { value: 3, label: '$2001 - $3000' },
+  { value: 4, label: '$3001 - $4000' },
+  { value: 5, label: '$4001 - $5000' },
+  { value: 6, label: 'more than 5001$' },
+];
+
+const ZIPCODE_OPTIONS = [
+  { value: '35801', label: 'Alabama 35801' },
+  { value: '99501', label: 'Alaska 99501' },
+  { value: '85001', label: 'Arizona 85001' },
+  { value: '94203', label: 'Arkansas 94203' },
+  { value: '90001', label: 'California 90001' },
+
+];
+
+const CUSTOMER_AGE_OPTIONS = [
+  { value: 1, label: 'less than 21' },
+  { value: 2, label: 'greater than 21' },
+];
+const PRODUCT_NAME_OPTIONS = [
+  { value: 1, label: 'TV' },
+  { value: 2, label: 'Washing Machine' },
+  { value: 3, label: 'Air Conditioner' },
+];
+const RENTAL_TENURE_OPTIONS = [
+  { value: [0,3], label: '0 - 3' },
+  { value: [3,6], label: '3 - 6' },
+  { value: [6,-1], label: 'more than 6 months' },
+
+];
 
 
 
-export {STRINGS};
+
+export {STRINGS,MONTHLY_RENTAL_AMOUNT_OPTIONS,ZIPCODE_OPTIONS,CUSTOMER_AGE_OPTIONS,PRODUCT_NAME_OPTIONS,RENTAL_TENURE_OPTIONS};
