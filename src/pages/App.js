ec26fa472f50bd4b2f1e65bf658d9b111bf853b4
abhikumar22 <abhi.ckp1002@gmail.com>
2020-05-09 14:23:38 +0530

    form added

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,65 +1,87 @@
 import React from "react";
 import "../assets/css/style.css";
 import { STRINGS } from "../utils/constants";
+import Select from 'react-select';
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+];
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      backgroundcolor:'transparent'
+      backgroundcolor: "transparent",
+      selectedOption: null,
     };
-    this.landingDiv = React.createRef()
-    this.homeDiv = React.createRef()
-    this.profileDiv = React.createRef()
+    this.landingDiv = React.createRef();
+    this.homeDiv = React.createRef();
+    this.profileDiv = React.createRef();
   }
 
+  handleChange = selectedOption => {
+    this.setState(
+      { selectedOption },
+      () => console.log(`Option selected:`, this.state.selectedOption)
+    );
+  };
+
   handleClick(type) {
-    console.log(type)
+    console.log(type);
     if (type === 0) {
       if (this.landingDiv.current) {
         this.landingDiv.current.scrollIntoView({
           behavior: "smooth",
-          block: "nearest"
-        })
+          block: "nearest",
+        });
       }
-    }
-    else if (type === 1) {
+    } else if (type === 1) {
       if (this.homeDiv.current) {
         this.homeDiv.current.scrollIntoView({
           behavior: "smooth",
-          block: "nearest"
-        })
+          block: "nearest",
+        });
       }
     } else {
       if (this.profileDiv.current) {
         this.profileDiv.current.scrollIntoView({
           behavior: "smooth",
-          block: "nearest"
-        })
+          block: "nearest",
+        });
       }
     }
   }
-  listenScrollEvent = e => {
+  listenScrollEvent = (e) => {
     if (window.scrollY > 100) {
-      console.log("> 400")
-      this.setState({backgroundColor: 'rgba(0, 0, 0, 0.8)'})
+      console.log("> 400");
+      this.setState({ backgroundColor: "rgba(0, 0, 0, 0.8)" });
     } else {
-      console.log("else")
-      this.setState({backgroundColor: 'transparent'})
+      console.log("else");
+      this.setState({ backgroundColor: "transparent" });
     }
-  }
+  };
 
   componentDidMount() {
-    window.addEventListener('scroll', this.listenScrollEvent)
+    window.addEventListener("scroll", this.listenScrollEvent);
   }
   render() {
     return (
       <div ref={this.landingDiv} className="first">
         <div className="bbg">
-          <nav className="navbar navbar-expand-lg navbar-dark fixed-top py-2" style={{backgroundColor:this.state.backgroundColor}}>
-            <h1 onClick={() => { this.handleClick(0) }} className="text-white cursorPointer">
-            <strong>{STRINGS.NAV_TITLE}</strong>  
-              </h1>
+          <nav
+            className="navbar navbar-expand-lg navbar-dark fixed-top py-2"
+            style={{ backgroundColor: this.state.backgroundColor }}
+          >
+            <h1
+              onClick={() => {
+                this.handleClick(0);
+              }}
+              className="text-white cursorPointer"
+            >
+              <strong>{STRINGS.NAV_TITLE}</strong>
+            </h1>
             <button
               className="navbar-toggler"
               type="button"
@@ -76,12 +98,20 @@ export default class App extends React.Component {
               id="navbarSupportedContent"
             >
               <ul className="navbar-nav ml-auto">
-                <li className="nav-item active cursorPointer mr-5" onClick={() => {
-                  this.handleClick(1)
-                }}>
+                <li
+                  className="nav-item active cursorPointer mr-5"
+                  onClick={() => {
+                    this.handleClick(1);
+                  }}
+                >
                   <h4 className="nav-link">{STRINGS.NAV_HOME}</h4>
                 </li>
-                <li className="nav-item active cursorPointer mr-0" onClick={() => { this.handleClick(2) }}>
+                <li
+                  className="nav-item active cursorPointer mr-0"
+                  onClick={() => {
+                    this.handleClick(2);
+                  }}
+                >
                   <h4 className="nav-link">{STRINGS.NAV_PROFILE}</h4>
                 </li>
               </ul>
@@ -89,11 +119,34 @@ export default class App extends React.Component {
           </nav>
         </div>
         <div ref={this.homeDiv} className="div2">
-        <div className="bbg"></div>
+          <div className="bbg text-black">
+            <div className="container h-100">
+              <div className="row align-items-center h-100">
+                <div className="col-10 col-sm-10 col-md-8 col-lg-6 mx-auto">
+                  <div className="formBorder py-5 px-5">
+                    <form className="w-100 text-center">
+                      <div className="form-group">
+                        <p>Monthly Rental Amount( $ ) :</p>
+                        <Select
+                          value={this.state.selectedOption}
+                          onChange={this.handleChange}
+                          options={options}
+                        />
+                        <p>Customer Age ( Years ) :</p>
+                        <p>Zipcode :</p>
+                        <p>Product Name :</p>
+                        <p>Rental Tenure :</p>
+                      </div>
+                      <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div ref={this.profileDiv} className="div3">
-        <div className="bbg"></div>
-
+          <div className="bbg"></div>
         </div>
       </div>
     );
